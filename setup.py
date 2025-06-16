dd21071a471d69dc5756543072ed0ceedfc449be
digo5ds <diogo.silva5ds@outlook.com>
2025-06-15 23:45:21 -0300

    fixing installer pacakge

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import find_packages, setup
 
+from pygitguard.__version__ import get_version
+
 setup(
     name="pygitguard",
-    version="0.1.0",
+    version=get_version(),
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "pygitguard=pygitguard_cli:main",
+            "pygitguard=pygitguard.cli:main",
         ],
     },
 )
