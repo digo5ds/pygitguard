b37f5ca7d46e490300c01a8a90b518d41b8bba0f
digo5ds <diogo.silva5ds@outlook.com>
2025-07-20 16:08:33 -0300

    standarize setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,18 @@ setup(
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "pygitguard=pygitguard.cli:main",
+            "pygitguard=pygitguard.cli:main ",
         ],
     },
+    author="Diogo Ferreira da Silva",
+    description="A Git security scanner that detects and prevents accidental commits of secrets, credentials, and other sensitive information during development.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/digo5ds/pygitguard",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
 )
